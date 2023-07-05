bfdc03abccf41b3f2d48305f0248c6e09f643dc7
Sven Seeberg <mail@sven-seeberg.de>
2023-07-05 16:47:15 +0200

    Add settings file for WSGI setup

--- integreat_compass/core/wsgi.py
+++ integreat_compass/core/wsgi.py
@@ -7,10 +7,39 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import configparser
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integreat_compass.core.settings")
 
-application = get_wsgi_application()
+def application(environ, start_response):
+    """
+    This returns the WSGI callable
+
+    :param environ: The environment variables
+    :type environ: dict
+
+    :param start_response: A function which starts the response
+    :type start_response: ~collections.abc.Callable
+
+    :return: The WSGI callable
+    :rtype: ~django.core.handlers.WSGIHandler
+    """
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integreat_compass.core.settings")
+
+    # Read config from config file
+    config = configparser.ConfigParser(interpolation=None)
+    config.read("/etc/integreat-compass.ini")
+    for section in config.sections():
+        for KEY, VALUE in config.items(section):
+            os.environ.setdefault(f"INTEGREAT_COMPASS_{KEY.upper()}", VALUE)
+
+    # Read config from environment
+    for key in environ:
+        if key.startswith("INTEGREAT_COMPASS_"):
+            os.environ[key] = environ[key]
+
+    _application = get_wsgi_application()
+
+    return _application(environ, start_response)
