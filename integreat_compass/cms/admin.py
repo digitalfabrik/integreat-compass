b6916c5e1f44472345a6ec2dd70a065dfb29d2e4
Sven Seeberg <mail@sven-seeberg.de>
2023-07-12 11:43:42 +0200

    Manage users via CRUD in production

--- integreat_compass/cms/admin.py
+++ integreat_compass/cms/admin.py
@@ -4,6 +4,7 @@ Debug lists and forms for all models
 from django.apps import apps
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 if settings.DEBUG:
     admin.site.register(apps.get_model("auth", "Permission"))
@@ -15,3 +16,5 @@ if settings.DEBUG:
         admin.site.register(model)
     for model in apps.get_app_config("sessions").get_models():
         admin.site.register(model)
+else:
+    admin.site.register(get_user_model())
