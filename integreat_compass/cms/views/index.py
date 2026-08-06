0726cf7714a5b94acf76163750eb5cf584f61d08
JoeyStk <johannes.stock@yahoo.de>
2026-08-06 15:37:20 +0200

    Chore: Refactor IndexListView.get() to reduce CC

--- integreat_compass/cms/views/index.py
+++ integreat_compass/cms/views/index.py
@@ -1,12 +1,12 @@
 from math import sqrt
 
 from django.conf import settings
-from django.db.models import Q
+from django.db.models import Prefetch, Q
 from django.shortcuts import render
 from django.views.generic import ListView
 
 from ..constants import offer_group_types, offer_mode_types
-from ..models import Offer, Tag
+from ..models import Comment, Offer, OfferVersion, Tag
 
 
 def within_radius(offer_lat, offer_long, lat, long, radius):
@@ -53,10 +53,138 @@ class IndexListView(ListView):
 
     def get_queryset(self):
         """
+        Fetches all offers together with the related data needed to render them,
+        so that iterating over the offers (and their versions) does not trigger
+        a separate query per offer (N+1).
+
         :return: The offer queryset
         :rtype: ~django.db.models.query.QuerySet
         """
-        return Offer.objects.all()
+        return Offer.objects.select_related(
+            "organization", "offer_contact", "location"
+        ).prefetch_related(
+            "tags",
+            Prefetch(
+                "versions",
+                queryset=OfferVersion.objects.select_related(
+                    "language"
+                ).prefetch_related(
+                    "documents",
+                    Prefetch(
+                        "comments", queryset=Comment.objects.select_related("creator")
+                    ),
+                ),
+            ),
+        ).order_by("id")
+
+    def _filter_by_tags(self, queryset, tags):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param tags: The tag ids to filter by
+        :type tags: list
+
+        :return: The filtered queryset
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        if not tags:
+            return queryset
+        return queryset.filter(tags__in=tags)
+
+    def _filter_by_mode(self, queryset, mode_types):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param mode_types: The mode types to filter by
+        :type mode_types: list
+
+        :return: The filtered queryset
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        if not mode_types:
+            return queryset
+        return queryset.filter(mode_type__in=mode_types)
+
+    def _filter_by_group(self, queryset, group_types):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param group_types: The group types to filter by
+        :type group_types: list
+
+        :return: The filtered queryset
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        if not group_types:
+            return queryset
+        return queryset.filter(group_type__in=group_types)
+
+    def _filter_by_search(self, queryset, search):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param search: The search term to filter by
+        :type search: str
+
+        :return: The filtered queryset
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        if not search:
+            return queryset
+        return queryset.filter(
+            Q(versions__title__icontains=search)
+            | Q(versions__description__icontains=search)
+        )
+
+    def _filter_by_free_offer(self, queryset, free_offer):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param free_offer: Whether to filter for free offers only
+        :type free_offer: bool
+
+        :return: The filtered queryset
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        if not free_offer:
+            return queryset
+        return queryset.filter(versions__is_free=True)
+
+    def _filter_by_radius(self, queryset, radius, lat, long):
+        """
+        :param queryset: The offer queryset to filter
+        :type queryset: ~django.db.models.query.QuerySet
+
+        :param radius: The max distance in km, as a string from the request
+        :type radius: str
+
+        :param lat: The latitude to filter by, as a string from the request
+        :type lat: str
+
+        :param long: The longitude to filter by, as a string from the request
+        :type long: str
+
+        :return: The offers within the given radius
+        :rtype: list
+        """
+        if not (radius and lat and long):
+            return queryset
+        return [
+            offer
+            for offer in queryset
+            if within_radius(
+                offer.location.lat,
+                offer.location.long,
+                float(lat),
+                float(long),
+                float(radius),
+            )
+        ]
 
     def get(self, request, *args, **kwargs):
         """
@@ -74,44 +202,27 @@ class IndexListView(ListView):
         :return: Response for filtered offers
         :rtype: ~django.template.response.TemplateResponse
         """
-        filtered_offers = Offer.objects.all()
-        if offer_tags := request.GET.getlist("tags"):
-            filtered_offers = filtered_offers.filter(tags__in=offer_tags)
-
-        if mode_types := request.GET.getlist("mode"):
-            filtered_offers = filtered_offers.filter(mode_type__in=mode_types)
-
-        if group_types := request.GET.getlist("group"):
-            filtered_offers = filtered_offers.filter(group_type__in=group_types)
+        offer_tags = request.GET.getlist("tags")
+        mode_types = request.GET.getlist("mode")
+        group_types = request.GET.getlist("group")
+        free_offers = bool(request.GET.get("free_offer"))
+        radius = request.GET.get("radius")
+        lat = request.GET.get("lat")
+        long = request.GET.get("long")
 
         if offer_search := request.GET.get("search"):
             offer_search = offer_search.strip()
-            filtered_offers = filtered_offers.filter(
-                Q(versions__title__icontains=offer_search)
-                | Q(versions__description__icontains=offer_search)
-            )
-        if free_offers := bool(request.GET.get("free_offer")):
-            filtered_offers = filtered_offers.filter(versions__is_free=True)
+
+        filtered_offers = self.get_queryset()
+        filtered_offers = self._filter_by_tags(filtered_offers, offer_tags)
+        filtered_offers = self._filter_by_mode(filtered_offers, mode_types)
+        filtered_offers = self._filter_by_group(filtered_offers, group_types)
+        filtered_offers = self._filter_by_search(filtered_offers, offer_search)
+        filtered_offers = self._filter_by_free_offer(filtered_offers, free_offers)
 
         # from here on out, take care to not duplicate offers!
         filtered_offers = filtered_offers.distinct()
-
-        if (
-            (radius := request.GET.get("radius"))
-            and (lat := request.GET.get("lat"))
-            and (long := request.GET.get("long"))
-        ):
-            filtered_offers = [
-                offer
-                for offer in filtered_offers
-                if within_radius(
-                    offer.location.lat,
-                    offer.location.long,
-                    float(lat),
-                    float(long),
-                    float(radius),
-                )
-            ]
+        filtered_offers = self._filter_by_radius(filtered_offers, radius, lat, long)
 
         return render(
             request,
